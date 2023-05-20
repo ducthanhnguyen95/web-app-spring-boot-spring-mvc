2a7492efdd6b5e9c68d01a3d51980d5383cf53b3
thanhnd <ducthanhnguyen95@gmail.com>
2023-05-20 23:22:29 +0700

    Implementing web apps with a dynamic view

---
+++ src/main/java/com/example/webappspringbootspringmvc/WebAppSpringBootSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappspringbootspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBootSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringBootSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webappspringbootspringmvc/WebAppSpringBootSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappspringbootspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSpringBootSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
